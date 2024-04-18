b5b47b118860ed3acc2cfde58a90247670bc9f88
Gabriel Henrique Speraceta <gabriel.hesperanceta@gmail.com>
2024-04-18 18:01:42 -0300

    update 1.03

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-import Swup from './node_modules/swup';
-const swup = new Swup({
-    containers: ["#swup"]
-  });
+
+const openAbout = document.querySelector('#openMoreAbout');
+
+openAbout.addEventListener('click', () => {
+    const moreOptions = document.querySelector('#moreOptionsAbout');
+    const hasToggle = moreOptions.classList.contains("toggle");
+    if(hasToggle) {
+        moreOptions.classList.remove("toggle");
+        moreOptions.classList.add("toggleOff");
+        moreOptions.style.display = "none";
+    }
+    else {
+        moreOptions.classList.add("toggle");
+        moreOptions.classList.remove("toggleOff");
+        moreOptions.style.display = "";
+    }
+});

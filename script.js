b970499df03de582671d8b2b3d959144ea289803
Gabriel Henrique Speraceta <gabriel.hesperanceta@gmail.com>
2024-04-18 22:39:06 -0300

    update 1.06

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-
+const singleTecElements = document.querySelectorAll('.singleTec');
 const openAbout = document.querySelector('#openMoreAbout');
+
+let previousColor = '#010101';
 
 openAbout.addEventListener('click', () => {
     const moreOptions = document.querySelector('#moreOptionsAbout');
@@ -15,3 +17,22 @@ openAbout.addEventListener('click', () => {
         moreOptions.style.display = "";
     }
 });
+
+singleTecElements.forEach((element, index) => {
+    const newColor = lightenColor(previousColor, 1);
+    element.style.backgroundColor = newColor;
+    previousColor = newColor;
+});
+
+function lightenColor(color, amount) {
+    let r = parseInt(color.substr(1, 2), 16);
+    let g = parseInt(color.substr(3, 2), 16);
+    let b = parseInt(color.substr(5, 2), 16);
+
+
+    r = Math.min(255, Math.round(r * (1 + amount)));
+    g = Math.min(255, Math.round(g * (1 + amount)));
+    b = Math.min(255, Math.round(b * (1 + amount)));
+
+    return `#${r.toString(16).padStart(2, '0')}${g.toString(16).padStart(2, '0')}${b.toString(16).padStart(2, '0')}`;
+}
